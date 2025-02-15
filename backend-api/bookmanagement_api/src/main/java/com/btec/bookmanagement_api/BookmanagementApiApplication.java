f6cd31a6c7986afc9689d722c5d1bd2750077c93
neuylduy24 <133001707+neuylduy24@users.noreply.github.com>
2025-02-15 19:59:35 +0700

    update code

---
+++ backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/BookmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.btec.bookmanagement_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagementApiApplication.class, args);
+	}
+
+}
---
+++ backend-api/bookmanagement_api/src/test/java/com/btec/bookmanagement_api/BookmanagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btec.bookmanagement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

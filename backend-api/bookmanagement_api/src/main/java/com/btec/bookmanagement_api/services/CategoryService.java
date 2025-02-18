1c3360764c9c21c52918181dd9aa4b96a583ddb4
neuylduy24 <133001707+neuylduy24@users.noreply.github.com>
2025-02-18 20:11:08 +0700

    update code

---
+++ backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/services/CategoryService.java
@@ -0,0 +1,49 @@
+package com.btec.bookmanagement_api.services;
+
+import com.btec.bookmanagement_api.entities.Category;
+import com.btec.bookmanagement_api.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(String id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Optional<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public boolean existsByName(String name) {
+        return categoryRepository.existsByName(name);
+    }
+
+    public Category createCategory(Category category) {
+        if (categoryRepository.existsByName(category.getName())) {
+            throw new RuntimeException("Category with name '" + category.getName() + "' already exists!");
+        }
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(String id, Category categoryDetails) {
+        return categoryRepository.findById(id).map(category -> {
+            category.setName(categoryDetails.getName());
+            return categoryRepository.save(category);
+        }).orElseThrow(() -> new RuntimeException("Category not found with id " + id));
+    }
+
+    public void deleteCategory(String id) {
+        categoryRepository.deleteById(id);
+    }
+}

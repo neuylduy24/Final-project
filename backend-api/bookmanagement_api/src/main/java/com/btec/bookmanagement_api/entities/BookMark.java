07b822621d910a052bfc674f4036f58861483ce0
neuylduy24 <133001707+neuylduy24@users.noreply.github.com>
2025-02-18 22:32:19 +0700

    update code

--- backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/BookMark.java
+++ backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/BookMark.java
@@ -1,4 +1,15 @@
 package com.btec.bookmanagement_api.entities;
 
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "bookmarks")
 public class BookMark {
+    @Id
+    private String id;
+    private String userId;
+    private String bookId;
+    private String chapterId;
+
 }
+

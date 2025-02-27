c2d15ca070df9b473aa55ff5b100fb49d4d0d20e
neuylduy24 <133001707+neuylduy24@users.noreply.github.com>
2025-02-27 22:56:10 +0700

    update code

--- backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/User.java
+++ backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/User.java
@@ -1,16 +1,26 @@
 package com.btec.bookmanagement_api.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.mongodb.connection.ProxySettings;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
-@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -23,63 +33,71 @@ public class User {
     private String password;
     @NotBlank(message = "Username is required")
     private String username;
-    private Role roleId;
+    Set<String> roles;
     private String avatar;
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Role getRoles() {
-        return roleId;
-    }
-
-    public void setRoles(Role roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public Set<String> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(Set<String> roles) {
+//        this.roles = roles;
+//    }
+//
+//    //    public Role getRoles() {
+////        return roleId;
+////    }
+////
+////    public void setRoles(Role roleId) {
+////        this.roleId = roleId;
+////    }
+//
+//    public String getAvatar() {
+//        return avatar;
+//    }
+//
+//    public void setAvatar(String avatar) {
+//        this.avatar = avatar;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
 }

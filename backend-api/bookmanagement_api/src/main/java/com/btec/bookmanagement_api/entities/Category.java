2994798b9389136227dff4038d6988db0b88cf14
neuylduy24 <duyluyent@gmail.com>
2025-03-28 19:57:16 +0700

    update code

--- backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/Category.java
+++ backend-api/bookmanagement_api/src/main/java/com/btec/bookmanagement_api/entities/Category.java
@@ -1,6 +1,7 @@
 package com.btec.bookmanagement_api.entities;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 public class Category {
     @Id
+    @NotNull(message = "Id is required")
+    @NotBlank(message = "Id cannot be blank")
     private String id;
+
     @NotBlank(message = "Category name is required")
     private String name;
 }

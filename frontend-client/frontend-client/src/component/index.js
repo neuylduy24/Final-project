d85e0aeecb11c6b16d193fd55328615647126f7d
trongcode <167011645+trongcode@users.noreply.github.com>
2025-04-03 21:05:34 +0700

    Delete frontend-client/frontend-client directory

--- frontend-client/frontend-client/src/component/index.js
+++
@@ -1,5 +0,0 @@
-import {default as Chapter} from './Chapter/chapter';
-import {default as Comment} from './Comment/comment';
-import {default as ButtonFollow} from './ButtonFollow/buttonFollow';
-
-export {Chapter, ButtonFollow, Comment};
